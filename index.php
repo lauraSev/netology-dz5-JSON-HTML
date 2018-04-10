6c674f2447e37afaa82851ba978e3776bf04116a
laura <severyuhina.lv@eksmo.ru>
2018-04-11 01:34:46 +0300

    dz5-json-html

---
+++ index.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>JSON-HTML</title>
+</head>
+
+<body>
+<pre>
+
+<?php
+	$json = file_get_contents ( 'json.json');
+	$json = json_decode ( $json, true);
+	//print_r ($json);
+	echo '<table border="1">
+			<tr>';
+	foreach ($json[0] as $key => $val )
+	{
+		echo '<th> ' . $key . ' </th>';
+	}
+	echo '</tr>';
+	
+	
+	foreach ($json as $key => $val ){
+		echo '<tr>';
+		foreach ($val as $about ){
+			echo '<td> ' . $about . '</td>';
+		}
+		echo '</tr>';
+	
+	}
+	echo '</table>'
+	
+	
+	
+	
+	
+		
+		
+?>
+
+</body>
+</html>
